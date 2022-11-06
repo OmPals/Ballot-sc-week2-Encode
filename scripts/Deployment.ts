a8caa02aaf4d4a11960b1a6770f5b1af152b8699
OmPals <ompalsanawala241099@gmail.com>
2022-11-06 18:20:12 +0530

    Ballot deployment

--- scripts/Deployment.ts
+++ scripts/Deployment.ts
@@ -11,31 +11,19 @@ function convertStringArrayToBytes32(array: string[]) {
 }
 
 async function main() {
-  // console.log("Deploying Ballot contract");
-  // console.log("Proposals: ");
-  // PROPOSALS.forEach((element, index) => {
-  //   console.log(`Proposal N. ${index + 1}: ${element}`);
-  // });
+  console.log("Deploying Ballot contract");
+  console.log("Proposals: ");
+  PROPOSALS.forEach((element, index) => {
+    console.log(`Proposal N. ${index + 1}: ${element}`);
+  });
   
-  // const ballotFactory = await ethers.getContractFactory("Ballot");
-  // const ballotContract = await ballotFactory.deploy(
-  //     convertStringArrayToBytes32(PROPOSALS)
-  // );
-  // await ballotContract.deployed();
+  const ballotFactory = await ethers.getContractFactory("Ballot");
+  const ballotContract = await ballotFactory.deploy(
+      convertStringArrayToBytes32(PROPOSALS)
+  );
+  await ballotContract.deployed();
 
-  // console.log("Ballot contract is deployed on: ", ballotContract.address);
-
-  const helloWorldFactory = await ethers.getContractFactory("HelloWorld");
-    // accounts = await ethers.getSigners();
-    // https://docs.ethers.io/v5/api/contract/contract-factory/#ContractFactory-deploy
-    const helloWorldContract = await helloWorldFactory.deploy();
-
-    console.log("HelloWorld contract is deploying on: ", helloWorldContract.address);
-
-    // https://docs.ethers.io/v5/api/contract/contract/#Contract-deployed
-    await helloWorldContract.deployed();
-
-    console.log("HelloWorld contract is deployed on: ", helloWorldContract.address);
+  console.log("Ballot contract is deployed on: ", ballotContract.address);
 }
 
 main().catch((error) => {
